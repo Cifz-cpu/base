966f0bb3106a2d71efc7448bb37aa346eff14fa5
cifz <wangzhen1798@gmail.com>
2018-04-27 07:02:40 +0800

    Initial commit

---
+++ datasoft.py
@@ -0,0 +1,19 @@
+# print('I\'m\"OK\"!')
+# print('\\\t\\')
+# print(r'\\\t\\')
+# print('''line1
+# ... line2
+# ... line3''')
+# print(3>2)
+# a = 123
+# print(a)
+# a = 'ABC'
+# print(a)
+# X = 10
+# X = X + 2
+# print(X)
+
+a = 'ABC'
+b = a
+a = 'XYZ'
+print(11//3)
