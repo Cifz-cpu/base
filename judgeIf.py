83fb4894edd9768a3a4741f67a17261631d7e6e1
cifz <wangzhen1798@gmail.com>
2018-04-27 22:11:42 +0800

    first commit

---
+++ judgeIf.py
@@ -0,0 +1,10 @@
+age = 3
+if age >= 18:
+    print('your are is',age)
+    print('adult')
+else:
+    print('your age is',age)
+    print("teeager")
+
+
+
---
+++ liststudy.py
@@ -0,0 +1,14 @@
+# classmates = ['ljm','zyq','wangzhuangbi','sfq','lyl']
+# print(classmates)
+# print(len(classmates))
+# print(classmates[-1])
+# classmates.append('haha')
+# print(len(classmates))
+# classmates.insert(2,'wzzz')
+#
+# classmates.pop(3)
+# print(classmates)
+s = ['php','java',['asp','python'],'c4']
+print(s[2][0])
+t = (1,2)
+print(t[0])
