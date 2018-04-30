756370249dfb55ebb20ce528651796a5286dbeb4
cifz <wangzhen1798@gmail.com>
2018-04-30 18:56:38 +0800

    study

---
+++ dictset.py
@@ -0,0 +1,4 @@
+d = {'wangzhen':100,'sfq':10,'lyl':15}
+print(d['wangzhen'])
+d['sfq'] = 56
+print(d['sfq'])
--- judgeIf.py
+++ judgeIf.py
@@ -1,10 +1,23 @@
-age = 3
-if age >= 18:
-    print('your are is',age)
-    print('adult')
-else:
-    print('your age is',age)
-    print("teeager")
+# age = 3
+# if age >= 18:
+#     print('your are is',age)
+#     print('adult')
+# else:
+#     print('your age is',age)
+#     print("teeager")
 
+# a = input('birth:')
+# birth = int(a)
+# if birth < 2000:
+#     print('00钱00')
+# else :
+#     print('00hou')
 
+classmates = ['wangzhen','sfq','lyl']
+for name in classmates:
+    print(name)
 
+sum = 0
+for x in range(10):
+    sum = sum + x
+print(sum)
