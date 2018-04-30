7bbf4bfa444b3d1162a802055f5f78c24091966e
cifz <wangzhen1798@gmail.com>
2018-04-30 23:44:32 +0800

    finish hanshu

--- hanshu.py
+++ hanshu.py
@@ -25,12 +25,44 @@ import math
 #
 # print(move(100,100,60,math.pi / 6))
 
-def power(x,n=2):
-    s = 1
-    while n>0:
-        s = x * s
-        n = n - 1
-    return s
+# def power(x,n=2):
+#     s = 1
+#     while n>0:
+#         s = x * s
+#         n = n - 1
+#     return s
+#
+# print(power(2))
 
-print(power(2))
+def add_end(L = None):
+    if L is None:
+        L = []
+    L.append('lxq')
+    return L
+# print(add_end())
+# print(add_end())
 
+# def calc (*numbers):
+#     sum = 0
+#     for n in numbers:
+#         sum = sum + n
+#     return sum
+# num = [1,2,3,4,5,6,7,8,9]
+# print(calc(*num))
+
+def person(name,age,**kw):
+
+    if 'city' in kw:
+        print('city')
+
+    print('name',name,'age',age,'other',kw)
+
+# person('wz',18,city='New York',job='ShiZhang')
+def fact_iter(n,pro):
+    if n==1:
+        return pro
+    return fact_iter(n-1,n * pro)
+
+def fact(n):
+    return fact_iter(n,1)
+print(fact(100))
